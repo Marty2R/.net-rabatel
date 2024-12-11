e30d1c00dc8ad0fae3b6bc7f2d6cfee52cf99a91
Marty Rabatel <marty.rabatel2@gmail.com>
2024-12-11 08:27:39 +0100

    update

--- projects/mvcTemplate/obj/Debug/net8.0/mvc.AssemblyInfo.cs
+++ projects/mvcTemplate/obj/Debug/net8.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bec3105a6bf24737646e67245e48b3107ae89fe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f4097533988c11206ba1b104fd634e1b8cef3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
